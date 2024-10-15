a5cd86f3bc20094757c8b54667def643aef07681
Guy Roux <treusnip89@gmail.com>
2024-10-15 11:19:43 +0200

    Prevent SQL injection

--- mfa.py
+++ mfa.py
@@ -7,18 +7,18 @@ import sqlite3
 import traceback
 import schedule
 
-PORT     = 465
-SMTP_SRV = "smtp.gmail.com"
-WEB_SRV  = "example.com"
-SENDER   = "TODO@gmail.com"
-PASSWORD = "TODO"
+DB_FILENAME    = 'mfa.db'
+PORT           = 465
+SMTP_SRV       = "smtp.gmail.com"
+WEB_SRV        = "example.com"
+SENDER         = "TODO@gmail.com"
+PASSWORD       = "TODO"
 VALID_DURATION = 10 # in minutes
-MESSAGE  = f"""Subject: MFA
+MESSAGE        = f"""Subject: MFA
 
 The following link will be valid for {VALID_DURATION} minutes.
 
 Link to validate the multi factor authentification : """
-DB_FILENAME = 'mfa.db'
 
 def timeout_mfa(_token):
 	con = sqlite3.connect(DB_FILENAME)
@@ -36,7 +36,7 @@ def mfa():
 			_mail = request.args['mail']
 			if 'token' in request.args:
 				_token_co = request.args['token']
-				print(_mail)
+
 				# Send mail with unique token
 				_token = str(uuid.uuid4())
 				url = f"{WEB_SRV}/validate?token={_token}"
@@ -49,11 +49,11 @@ def mfa():
 				con = sqlite3.connect(DB_FILENAME)
 				cur = con.cursor()
 				cur.execute('CREATE TABLE IF NOT EXISTS mfa ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "mail" TEXT, "token" TEXT, "token_co" TEXT)')
-				res = cur.execute(f"SELECT * FROM mfa WHERE mail='{_mail}' AND token_co='{_token_co}'")
+				res = cur.execute("SELECT * FROM mfa WHERE mail=? AND token_co=?", (_mail, _token_co))
 				f = res.fetchone()
-				print(f)
+
 				if f is None:
-					cur.execute(f"INSERT INTO mfa(mail,token, token_co) VALUES ('{_mail}', '{_token}', '{_token_co}')")
+					cur.execute("INSERT INTO mfa(mail,token, token_co) VALUES (?, ?, ?)", (_mail, _token, _token_co))
 				else:
 					cur.execute(f"UPDATE mfa SET token='{_token}' WHERE id={f[0]}")
 				con.commit()
@@ -75,6 +75,7 @@ def validate():
 		schedule.run_pending()
 		if 'token' in request.args:
 			_token = request.args['token']
+
 			con = sqlite3.connect(DB_FILENAME)
 			cur = con.cursor()
 			res = cur.execute(f"SELECT token_co FROM mfa WHERE token='{_token}'")
